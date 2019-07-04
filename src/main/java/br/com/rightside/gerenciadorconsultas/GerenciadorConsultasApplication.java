d2988898cafd63e5d1ef027b6b8d72665e84a395
Vagner Oliveira <ti.vagner@gmail.com>
2019-07-04 17:24:12 -0300

    Primeiro commit, configuração ok

---
+++ src/main/java/br/com/rightside/gerenciadorconsultas/GerenciadorConsultasApplication.java
@@ -0,0 +1,13 @@
+package br.com.rightside.gerenciadorconsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorConsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorConsultasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rightside/gerenciadorconsultas/GerenciadorConsultasApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.rightside.gerenciadorconsultas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GerenciadorConsultasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
